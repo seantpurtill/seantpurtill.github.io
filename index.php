eb6f2de68c2b4b7fa5f4f2baf06b858d40bb15de
ACDM\764092 <sean.purtill@edu.sait.ca>
2018-02-11 20:34:44 -0700

    bootstrap for page3

---
+++ index.php
@@ -0,0 +1,355 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Portfolio &mdash; Sean Purtill</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FreeHTML5.co
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body style="background-image: url(images/bg_img.jpg);">
+	
+	<div id="fh5co-main">
+		<div class="fh5co-tab-wrap">
+			<ul class="fh5co-tab-menu">
+				<li class="active"><a href="#" data-tab="1"><span class="icon icon-glass"></span><span class="menu-text">Profile</span></a></li>
+				<li><a href="#" data-tab="2"><span class="icon icon-graduation-cap"></span><span class="menu-text">Education</span></a></li>
+				<li><a href="#" data-tab="3"><span class="icon icon-black-tie"></span><span class="menu-text">Experience</span></a></li>
+				<li><a href="#" data-tab="4" data-pie="yes"><span class="icon icon-fighter-jet"></span><span class="menu-text">Past Projects</span></a></li>
+			</ul>
+			<div class="fh5co-tab-content active" data-content="1">
+				<div class="fh5co-content-inner text-center">
+					<div class="row row-bottom-padded-sm">
+						<div class="col-md-12">
+							<figure>
+								<img src="smaller%20headshot.PNG" alt="Headshot of Sean Purtill">
+							</figure>
+							<h1>Sean Purtill</h1>
+                        <p>Looking for work in Calgary. This portfolio is in development, and not everything is currently in working order. However, I would rather have my MVP(minimum viable product) up and running than waiting until after I get a job before publishing. Thanks for visiting and feel free to leave advice, and let's not beat around the bush: ask me for an interview! 
+                                
+                             </p>
+						</div>
+					</div>
+<!--
+					<div class="row">
+						<div class="col-md-12 fh5co-counter">
+							<div class="fh5co-number fh5co-left">1</div>
+							<div class="fh5co-text">
+								<h3 class="border-bottom">Year of experience in Software development</h3>
+								
+							</div>
+						</div>
+					</div>
+-->
+					<ul class="fh5co-social">
+						<li><a href="#"><i class="icon-envelope"></i></a></li>
+						<li><a href="#"><i class="icon-twitter"></i></a></li>
+						<li><a href="#"><i class="icon-instagram"></i></a></li>
+						<li><a href="#"><i class="icon-google"></i></a></li>
+						<li><a href="#"><i class="icon-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="fh5co-tab-content" data-content="2">
+				<div class="fh5co-content-inner">
+					<div class="row">
+						<div class="col-md-12">
+							<p>I'm proud to be a graduate of these institutions.</p>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-graduation-cap"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2017-2018</h2>
+									<p>SAIT (Southern Alberta Institute of Technology)</p>
+                                    <p>Object Oriented Software Development</p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-graduation-cap"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2006-2010</h2>
+									<p>Trent University</p>
+                                    <p>Joint Honours Bachelor's Degree in Business Administration and <br/> Philosophy with an Emphasis in Applied Ethics</p>
+								</div>
+							</div>
+
+							
+                            
+<!--
+                            
+                            https://www.w3schools.com/howto/howto_css_timeline.asp
+-->
+
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="fh5co-tab-content" data-content="3">
+				<div class="fh5co-content-inner">
+<!--
+					<div class="row">
+						<div class="col-md-12">
+
+							<p>I have the privilege to work with these cool companies.</p>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-black-tie"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2014 - 2017</h2>
+									<h3>Winsport</h3>
+                                    <h3>Calgary, Alberta</h3>
+									<p>Event Manager</p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-black-tie"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2014</h2>
+									<h3>Good Shepherd Eye Clinic</h3>								<h3>Swaziland, Southern Africa</h3>
+									<p>Project Coordinator</p>
+								</div>
+							</div>
+
+							
+
+						</div>
+						
+					</div>
+-->
+
+
+<div class="timeline">
+  <div class="timecontainer left">
+    <div class="content">
+      <h3>Winsport. Calgary, Alberta</h3>
+        <h3>2014 - 2017</h3>
+        <h3>Event Set Up Manager</h3>
+      <p>Managed thousands of event set ups and tear downs, lead a team of 5 
+          Click here to read more about this role
+          
+         Hidden: ~ As Set up Team Lead, I coordinated the on-site logistics with the Specia
+Events department for small corporate events to 3000+ guest festivals.
+~ Drawing on my 10+ years of experience in the hospitality industry, this
+challenging role is a combination of customer service, operations
+management, project management and supervising teams of ranging from 3
+to 20 staff
+~ Transitioned 3 on-site restaurant facilities from using disposable cloths to
+kitchen cloths washable in-house, saving 90% in costs, and vastly reducing
+unnecessary waste
+~ Designing an intricately welded frame for moving chairs increasing work
+speed by 300%, particularly useful during time-critical events </p>
+    </div>
+  </div>
+  <div class="timecontainer right">
+    <div class="content">
+        <h2>Good Shepherd Eye Clinic, Swaziland</h2>
+        <h2>2013</h2>
+        <h2>Project Coordinator</h2>
+            <p>Managed adminstrative and IT projects in Swaziland at a rural clinic
+               Click here to read more about this role 
+                
+~ Worked the medical and clerical staff to modernize the administrative side of the of the clinic operations, focusing on patient data management, procurement, inventory management, and administrative accounting
+~ Coordinated the launch of a patient database in three rural African clinics, including government hospitals, across Swaziland, which transitioned handwritten medical records to electronic records
+</p>
+    </div>
+  </div>
+  <div class="timecontainer left">
+    <div class="content">
+      <h2>2015</h2>
+      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    </div>
+  </div>
+  <div class="timecontainer right">
+    <div class="content">
+      <h2>2012</h2>
+      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    </div>
+  </div>
+  <div class="timecontainer left">
+    <div class="content">
+      <h2>2011</h2>
+      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    </div>
+  </div>
+  <div class="timecontainer right">
+    <div class="content">
+      <h2>2007</h2>
+      <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+    </div>
+  </div>
+</div>
+
+                    
+</div>
+                </div>
+                    
+                    
+                    
+				
+<div class="fh5co-tab-content" data-content="2">
+				<div class="fh5co-content-inner">			
+		<!-- Images used to open the lightbox -->
+<div class="row">
+  <div class="column">
+    <img style="timeimg" src="images/download%20(4).jpg" onclick="openModal();currentSlide(1)" class="hover-shadow">
+  </div>
+  <div class="column">
+    <img src="images/images%20(16).jpg" onclick="openModal();currentSlide(2)" class="hover-shadow">
+  </div>
+  <div class="column">
+    <img src="images/images%20(6).jpg" onclick="openModal();currentSlide(3)" class="hover-shadow">
+  </div>
+  <div class="column">
+    <img src="images/monet-garden%20archway.jpg" onclick="openModal();currentSlide(4)" class="hover-shadow">
+  </div>
+</div>
+
+<!-- The Modal/Lightbox -->
+<div id="myModal" class="modal">
+  <span class="close cursor" onclick="closeModal()">&times;</span>
+  <div class="modal-content">
+
+    <div class="mySlides">
+      <div class="numbertext">1 / 4</div>
+      <img src="images/download%20(4).jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">2 / 4</div>
+      <img src="images/images%20(16).jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">3 / 4</div>
+      <img src="images/images%20(6).jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">4 / 4</div>
+      <img src="images/monet-garden%20archway.jpg" style="width:100%">
+    </div>
+
+    <!-- Next/previous controls -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <!-- Caption text -->
+    <div class="caption-container">
+      <p id="caption"></p>
+    </div>
+
+    <!-- Thumbnail image controls -->
+    <div class="column">
+      <img class="demo" src="images/download%20(4).jpg" onclick="currentSlide(1)" alt="Park">
+    </div>
+
+    <div class="column">
+      <img class="demo" src="images/images%20(16).jpg" onclick="currentSlide(2)" alt="road">
+    </div>
+
+    <div class="column">
+      <img class="demo" src="images/images%20(6).jpg" onclick="currentSlide(3)" alt="Mountains">
+    </div>
+
+    <div class="column">
+      <img class="demo" src="images/monet-garden%20archway.jpg" onclick="currentSlide(4)" alt="Garden">
+    </div>
+  </div>
+  </div>
+ </div>
+</div>
+		<footer id="fh5co-footer">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<small>
+<!--
+						&copy; 2016 Personal Free HTML5 Template. All Rights Reserved. <span class="fh5co-author">
+						Designed by <a href="http://freehtml5.co" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></span>
+-->
+                        thank you for visiting my online portfolio. Feel free to download my resume by following <a href="Sean%20Purtill's%20Resume.pdf"> this link</a>, or send me an email by <a href="mailto:seandoescode@gmail.com?Subject=Hello,%20Sean." target="_top">clicking here</a>.
+					</small>
+				</div>
+			</div>
+		</footer>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Easy PieChart -->
+	<script src="js/jquery.easypiechart.min.js"></script>
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
